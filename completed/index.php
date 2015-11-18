4161042ecfb5b831d06c400da1a17f531d3e6d31
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-11-18 20:55:49 +0000

    Initial commit

---
+++ completed/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>jQuery Print Area</title>
+<meta name="description" content="jQuery Print Area" />
+<meta name="keywords" content="jQuery Print Area" />
+<meta http-equiv="imagetoolbar" content="no" />
+<link href="/css/core.css" rel="stylesheet" media="screen" type="text/css" />
+<link href="/css/core.css" rel="stylesheet" media="print" type="text/css" />
+</head>
+<body>
+
+<div id="wrapper">
+	
+	<div id="header">
+		<img src="/images/header.png" alt="header" width="800" height="150" />
+	</div>
+	
+	
+	<div id="content">
+	
+		
+		<h1>Heading content</h1>
+		
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam sit amet elit vitae arcu interdum ullamcorper. Nullam ultrices, nisi quis scelerisque convallis, augue neque tempor enim, et mattis justo nibh eu elit. Quisque ultrices gravida pede. Mauris accumsan vulputate tellus. Phasellus condimentum bibendum dolor. Mauris sed ipsum. Phasellus in diam. Nam sapien ligula, consectetuer id, hendrerit in, cursus sed, leo. Nam tincidunt rhoncus urna. Aliquam id massa ut nibh bibendum imperdiet. Curabitur neque mauris, porta vel, lacinia quis, placerat ultrices, orci.</p>
+
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In lectus nisl, porttitor eget, fermentum et, viverra a, odio. Sed semper. Praesent vulputate nibh eget nisi. Sed ligula. Aenean vehicula posuere neque. Nam nibh lectus, laoreet non, convallis sed, vulputate consectetuer, enim. Pellentesque arcu. Vestibulum consequat nisi a ipsum vulputate vehicula. Aliquam lacus diam, ultricies vel, interdum eget, posuere non, tellus. Donec vel ligula. Sed felis.</p>
+
+
+	<h2>Sub section heading</h2>
+	
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam sit amet elit vitae arcu interdum ullamcorper. Nullam ultrices, nisi quis scelerisque convallis, augue neque tempor enim, et mattis justo nibh eu elit. Quisque ultrices gravida pede. Mauris accumsan vulputate tellus. Phasellus condimentum bibendum dolor. Mauris sed ipsum. Phasellus in diam. Nam sapien ligula, consectetuer id, hendrerit in, cursus sed, leo. Nam tincidunt rhoncus urna. Aliquam id massa ut nibh bibendum imperdiet. Curabitur neque mauris, porta vel, lacinia quis, placerat ultrices, orci.</p>
+
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In lectus nisl, porttitor eget, fermentum et, viverra a, odio. Sed semper. Praesent vulputate nibh eget nisi. Sed ligula. Aenean vehicula posuere neque. Nam nibh lectus, laoreet non, convallis sed, vulputate consectetuer, enim. Pellentesque arcu. Vestibulum consequat nisi a ipsum vulputate vehicula. Aliquam lacus diam, ultricies vel, interdum eget, posuere non, tellus. Donec vel ligula. Sed felis.</p>
+
+
+
+	
+	</div>
+	
+	<a href="#" class="print" rel="content">Print</a>
+	
+</div>
+
+<script src="/js/jquery-1.6.2.min.js"></script>
+<script src="/js/jquery.PrintArea.js_4.js"></script>
+<script src="/js/core.js"></script>
+</body>
+</html>
+
+
+
+
---
+++ start/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>jQuery Print Area</title>
+<meta name="description" content="jQuery Print Area" />
+<meta name="keywords" content="jQuery Print Area" />
+<meta http-equiv="imagetoolbar" content="no" />
+<link href="/css/core.css" rel="stylesheet" media="screen" type="text/css" />
+<link href="/css/core.css" rel="stylesheet" media="print" type="text/css" />
+</head>
+<body>
+
+
+
+<script src="/js/jquery-1.6.2.min.js"></script>
+<script src="/js/jquery.PrintArea.js_4.js"></script>
+<script src="/js/core.js"></script>
+</body>
+</html>
